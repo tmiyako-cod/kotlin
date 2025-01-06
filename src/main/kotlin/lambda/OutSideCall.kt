c49cae884786a25269381062590ee17f0e992845
tappei.miyako <t_miyako@codmon.co.jp>
2025-01-07 08:35:25 +0900

    lambda out side call

---
+++ src/main/kotlin/lambda/OutSideCall.kt
@@ -0,0 +1,21 @@
+package org.example.lambda
+
+fun <T> List<T>.customFilter(predicate: (T) -> Boolean): List<T> {
+    val result = mutableListOf<T>()
+    for (item in this) {
+        if (predicate(item)) {
+            result.add(item)
+        }
+    }
+    return result
+}
+
+fun main() {
+    val list = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
+    val result = list.customFilter({ it % 2 == 0})
+    println(result)
+
+    val result2 = list.customFilter { it % 2 != 0 }
+    println(result2)
+}
+
