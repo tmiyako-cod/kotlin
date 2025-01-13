192405a4c291fbba876a36cb4cc71916df5a93bd
tappei.miyako <t_miyako@codmon.co.jp>
2025-01-13 12:18:39 +0900

    awaitSingle 2

--- src/main/kotlin/project_reactor/mono/awaitSingle.kt
+++ src/main/kotlin/project_reactor/mono/awaitSingle.kt
@@ -1,12 +1,12 @@
 package org.example.project_reactor.mono
 
-import kotlinx.coroutines.reactor.awaitSingle
+import kotlinx.coroutines.reactive.awaitSingle
 import kotlinx.coroutines.runBlocking
 import reactor.core.publisher.Mono
 
 fun main() = runBlocking {
     // Kotlin Coroutinesとの統合用拡張関数
-    // 非同期のMonoやFluxから1つの値を待機して取得するための拡張関数
+    // 非同期のMonoやFluxから1つの値を同期的に待機して取得するための拡張関数
     val mono = Mono.just("Hello, World!")
     val result = mono.awaitSingle()
     println(result)
