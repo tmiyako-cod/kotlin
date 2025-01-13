3072aeb5650c707f2ef147075d0075ceda3d36ba
tappei.miyako <t_miyako@codmon.co.jp>
2025-01-13 09:22:24 +0900

    generics 2

--- src/main/kotlin/generics/Generics.kt
+++ src/main/kotlin/generics/Generics.kt
@@ -7,4 +7,10 @@ fun main() {
     val generics2 = Generics(1)
     println(generics.value)
     println(generics2.value)
+
+    val generics3 = Generics(Generics("Hello"))
+    println(generics3.value.value)
+
+    val generics4 = Generics<String>("Hello")
+    println(generics4.value.uppercase())
 }
